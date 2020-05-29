2abd411245143478d5b0d31af41ddeee33e5b135
markinthecasbah <markus.mcmillan@gmail.com>
2020-05-29 16:02:55 -0400

    added basic third person controller implementation

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    public class InputHandler : MonoBehaviour
+    {
+        float horizontal;
+        float vertical;
+
+        bool aimInput;
+        bool sprintInput;
+        bool shootInput;
+        bool crouchInput;
+        bool reloadInput;
+        bool switchInput;
+        bool pivotInput;
+
+        bool isInit;
+
+        float delta;
+
+        public StatesManager states;
+        public Transform camHolder;
+
+        void Start()
+        {
+            InitInGame();
+        }
+
+        public void InitInGame()
+        {
+            states.Init();
+            isInit = true;
+        }
+
+        void FixedUpdate()
+        {
+            if (!isInit)
+                return;
+
+            delta = Time.fixedDeltaTime;
+            GetInput_FixedUpdate();
+            InGame_UpdateStates_FixedUpdate();
+            states.FixedTick(delta);
+        }
+
+        void GetInput_FixedUpdate()
+        {
+            vertical = Input.GetAxis("Vertical");
+            horizontal = Input.GetAxis("Horizontal");
+        }
+
+        void InGame_UpdateStates_FixedUpdate()
+        {
+            states.inp.horizontal = horizontal;
+            states.inp.vertical = vertical;
+            states.inp.moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+
+            Vector3 moveDir = camHolder.forward * vertical;
+            moveDir += camHolder.right * horizontal;
+            moveDir.Normalize();
+            states.inp.moveDirection = moveDir;
+
+        }
+
+        void Update()
+        {
+            if (!isInit)
+                return;
+
+            delta = Time.deltaTime;
+
+            states.Tick(delta);
+        }
+
+
+    }
+
+    public enum GamePhase
+    {
+        inGame,inMenu
+    }
+}
---
+++ Assets/Scripts/StatesManager.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    public class StatesManager : MonoBehaviour
+    {
+        public ControllerStats stats;
+        public ControllerStates states;
+        public InputVariables inp;
+
+        
+
+        [System.Serializable]
+        public class InputVariables
+        {
+            public float horizontal;
+            public float vertical;
+            public float moveAmount;
+            public Vector3 moveDirection;
+            public Vector3 aimPosition;
+        }
+
+        [System.Serializable]
+        public class ControllerStates
+        {
+            public bool onGround;
+            public bool isAiming;
+            public bool isCrouching;
+            public bool isRunning;
+            public bool isInteracting;
+        }
+    
+        public Animator anim;
+        public GameObject activeModel;
+        [HideInInspector]
+        public Rigidbody rigid;
+        [HideInInspector]
+        public Collider controllerCollider;
+
+        List<Collider> ragdollColliders = new List<Collider>();
+        List<Rigidbody> ragdollRigids = new List<Rigidbody>();
+        [HideInInspector]
+        public LayerMask ignoreLayers;
+        [HideInInspector]
+        public LayerMask ignoreForGround;
+
+        public Transform mTransform;
+        public CharState curState;
+        public float delta;
+
+        public void Init()
+        {
+            mTransform = this.transform;
+            SetupAnimator();
+            rigid = GetComponent<Rigidbody>();
+            rigid.isKinematic = false;
+            rigid.drag = 4;
+            rigid.angularDrag = 999;
+            rigid.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+            controllerCollider = GetComponent<Collider>();
+
+            SetupRagdoll();
+
+            ignoreLayers = ~(1 << 9);
+            ignoreForGround = ~(1 << 9 | 1 << 10);
+        }
+
+        void SetupAnimator()
+        {
+            if(activeModel == null)
+            {
+                anim = GetComponentInChildren<Animator>();
+                activeModel = anim.gameObject;
+            }
+
+            if (anim == null)
+                anim = activeModel.GetComponent<Animator>();
+
+            anim.applyRootMotion = false;
+        }
+
+        void SetupRagdoll()
+        {
+            Rigidbody[] rigids = activeModel.GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody r in rigids)
+            {
+                if(r == rigid)
+                {
+                    continue;
+                }
+
+                Collider c = r.gameObject.GetComponent<Collider>();
+                c.isTrigger = true;
+                ragdollRigids.Add(r);
+                ragdollColliders.Add(c);
+                r.isKinematic = true;
+                r.gameObject.layer = 10; 
+            }
+        }
+
+        public void FixedTick(float d)
+        {
+            delta = d;
+            switch (curState)
+            {
+                case CharState.normal:
+                    states.onGround = OnGround();
+                    if (states.isAiming)
+                    {
+
+                    }
+                    else
+                    {
+                        MovementNormal();
+                        RotationNormal();
+                    }
+                    break;
+                case CharState.onAir:
+                    rigid.drag = 0;
+                    states.onGround = OnGround();
+                    break;
+                case CharState.cover:
+                    break;
+                case CharState.vaulting:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void MovementNormal()
+        {
+            if (inp.moveAmount > 0.05f)
+                rigid.drag = 0;
+            else
+                rigid.drag = 4;
+
+            float speed = stats.walkSpeed;
+            if (states.isRunning)
+                speed = stats.runSpeed;
+            if (states.isCrouching)
+                speed = stats.crouchSpeed;
+
+            Vector3 dir = Vector3.zero;
+            dir = mTransform.forward * (speed * inp.moveAmount);
+            rigid.velocity = dir;
+        }
+
+        void RotationNormal()
+        {
+            Vector3 targetDir = inp.moveDirection;
+            targetDir.y = 0;
+
+            if (targetDir == Vector3.zero)
+                targetDir = mTransform.forward;
+
+            Quaternion lookDir = Quaternion.LookRotation(targetDir);
+            Quaternion targetRot = Quaternion.Slerp(mTransform.rotation, lookDir, stats.rotateSpeed * delta);
+            mTransform.rotation = targetRot;
+        }
+
+        void HandleAnimationsNormal()
+        {
+            float anim_v = inp.moveAmount;
+            anim.SetFloat("vertical", anim_v, 0.15f, delta);
+        }
+
+        void MovementAiming()
+        {
+
+        }
+
+        public void Tick(float d)
+        {
+            delta = d;
+
+            switch (curState)
+            {
+                case CharState.normal:
+                    states.onGround = OnGround();
+                    HandleAnimationsNormal();
+                    break;
+                case CharState.onAir:
+                    states.onGround = OnGround();
+                    break;
+                case CharState.cover:
+                    break;
+                case CharState.vaulting:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        bool OnGround()
+        {
+            Vector3 origin = mTransform.position;
+            origin.y += 0.6f;
+            Vector3 dir = -Vector3.up;
+            float dis = 0.7f;
+            RaycastHit hit;
+            if(Physics.Raycast(origin,dir,out hit, dis, ignoreForGround))
+            {
+                Vector3 tp = hit.point;
+                mTransform.position = tp;
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+
+    public enum CharState
+    {
+        normal,onAir,cover,vaulting
+    }
+}
