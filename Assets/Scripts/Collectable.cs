f458d2eec84984dd7eeb31a6ad6952e4131dfec5
markinthecasbah <markus.mcmillan@gmail.com>
2020-05-29 20:03:12 -0400

    added collectable object script, also allows for popup prompt if text mesh pro asset is added. Adds collected status on said item, currently only factoring for one collectable

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private bool collected = false;
+    private GameObject player;
+
+    public bool showLabel;
+    public GameObject textLabel;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (textLabel != null)
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) < 2f)
+            {
+                textLabel.gameObject.SetActive(true);
+            }
+            else
+            {
+                textLabel.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void Collect()
+    {
+        //set condition for 'good' ending, currently only 1 collectable
+        collected = true;
+        Destroy(this.gameObject);
+    }
+}
