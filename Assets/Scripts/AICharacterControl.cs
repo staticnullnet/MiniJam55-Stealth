6d76634d6d0427a85f8598d2a37c0a835cf4345a
Sky <skylerlusc@gmail.com>
2020-05-30 19:58:34 -0400

    Added janky patrol and finished turning AI

--- Assets/Scripts/AICharacterControl.cs
+++ Assets/Scripts/AICharacterControl.cs
@@ -1,46 +1,77 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
+
+    public enum GuardType
+    {
+        Idle,
+        Turn,
+        Patrol
+    }
+
     [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
     [RequireComponent(typeof (ThirdPersonCharacter))]
     public class AICharacterControl : MonoBehaviour
     {
+
+        [SerializeField] GuardType guardType;
+
         public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
         public ThirdPersonCharacter character { get; private set; } // the character we are controlling
-        public Transform target;                                    // target to aim for
-
-        public Transform player;                                    // player reference
-        public float angleToPlayer;
+        
+        
+        Transform player; // player reference
+        Transform target; // target to aim for
+        
+        
         [SerializeField] GameObject alertIndicator;
         [SerializeField] float enemyFOV = 60;
         [SerializeField] float maxDistance = 10f;
+        [SerializeField] float rotationSpeed = 1000f;
+        [SerializeField] float minDistance = 0.5f;
+        
+        [SerializeField] float moveDelay = 5f;        
+        private int destPoint = 0;
+        private bool isMoving = false;
+        public Transform[] points;
+        float angleToPlayer;
+
 
         private void Start()
         {
             // get the components on the object we need ( should not be null due to require component so no need to check )
             agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
+            player = GameObject.FindGameObjectWithTag("Player").transform;
 
             agent.updateRotation = false;
 	        agent.updatePosition = true;
+                        
         }
 
 
         private void Update()
         {
-            angleToPlayer = AngleToPlayer();
-            if (angleToPlayer <= enemyFOV && ClearVisionCheck())
-            {
-                alertIndicator.SetActive(true);
-            }
-            else
-            {
-                alertIndicator.SetActive(false);
-            }
+            IsPlayerVisible();
 
-
+            switch (guardType)
+            {
+                case GuardType.Idle:
+                    break;                
+                case GuardType.Turn:
+                    if (!isMoving)
+                        TurnToNextPoint();
+                    break;
+                case GuardType.Patrol:
+                        Patrol();
+                    break;
+            }
+            
+            
+            //pre baked chase-related commands in AI controller.
             if (target != null)
                 agent.SetDestination(target.position);
 
@@ -49,38 +80,80 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             else
                 character.Move(Vector3.zero, false, false);
         }
+   
+        private void Patrol()
+        {
+            isMoving = true;
 
-        //private bool ClearVisionCheck()
-        //{
-        //    Ray rayToPlayer = new Ray(transform.position, player.transform.position - transform.position);
-        //    LayerMask playerLayerMask = LayerMask.NameToLayer("Player");
-        //    RaycastHit hit;
-        //    bool result = false;
+            // Returns if no points have been set up
+            if (points.Length == 0)
+            return;
 
-        //    Debug.DrawRay(transform.position, player.transform.position - transform.position);
 
-        //    result = Physics.Raycast(rayToPlayer, out hit, maxDistance, playerLayerMask);
+            //if (agent.remainingDistance > agent.stoppingDistance)
+            //{
+            //    character.Move(agent.desiredVelocity, false, false);
+            //}
+            //else
+            //{
+            //    character.Move(Vector3.zero, false, false);
+            //}
 
-        //    if (result)
-        //        Debug.Log(hit.transform.name);
 
-        //    return result;
-        //}
+            if (Vector3.Distance(transform.position, points[destPoint].transform.position) <= minDistance)
+            {                
+                StartCoroutine(WaitThenMoveToNextWaypoint());
+                isMoving = false;                
+            }            
+            else
+            {   
+                character.Move(points[destPoint].transform.position - transform.position, false, false);
+            }
+            
+        }
 
-        private bool ClearVisionCheck()
+        IEnumerator WaitThenMoveToNextWaypoint()
+        {
+                Debug.Log("waiting");
+            yield return new WaitForSeconds(moveDelay);
+            destPoint = (destPoint + 1) % points.Length;
+                Debug.Log(destPoint);
+                yield break;
+        }
+
+        private bool IsPlayerVisible()
+            {
+                bool isPlayerVisible = false;
+                angleToPlayer = AngleToPlayer();
+                bool isInsideEnemyFOV = angleToPlayer <= enemyFOV;            
+
+                if (isInsideEnemyFOV && IsVisionToPlayerUnobstructed())
+                {
+                    alertIndicator.SetActive(true);
+                    isPlayerVisible = true;
+                }
+                else
+                {
+                    alertIndicator.SetActive(false);
+                }
+
+                return isPlayerVisible;
+            }
+
+        private bool IsVisionToPlayerUnobstructed()
         {
             Ray rayToPlayer = new Ray(transform.position, player.transform.position - transform.position);
             LayerMask playerLayerMask = LayerMask.NameToLayer("Player");
             RaycastHit hit;
             bool result = false;
 
-            Debug.DrawRay(transform.position, player.transform.position - transform.position);
+            //Debug.DrawRay(transform.position, player.transform.position - transform.position);
 
             result = Physics.Raycast(rayToPlayer, out hit, maxDistance);
 
             if (result)
             {
-                Debug.Log(hit.transform.name + " " + hit.collider.tag);
+                // Debug.Log(hit.transform.name + " " + hit.collider.tag);
 
                 if (hit.collider.tag != "Player")
                 {
@@ -96,18 +169,56 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             this.target = target;
         }
 
-        /// <summary>
-        /// Calculate direction between player and NPC
-        /// Get forward vector of enemy
-        /// Calculate angle (V3.angle)
-        /// Check angle w/ enemy FOV.
-        /// Ray cast to check walls
-        /// </summary>
+    
         private float AngleToPlayer()
         {
             Vector3 directionToPlayer = (player.transform.position - this.transform.position).normalized;
             float angle = Vector3.Angle(transform.forward, directionToPlayer);
             return angle;
+        }
+        
+
+        IEnumerator TurnToPoint(Quaternion finalRotation, float duration)
+        {           
+            isMoving = true;            
+            float t = 0;          
+                        
+            while (t < 1)
+            {
+                t += Time.smoothDeltaTime / duration;
+
+                transform.rotation = Quaternion.Lerp(transform.rotation, finalRotation, t);
+
+                //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(to), Time.deltaTime * t);
+
+                if (Quaternion.Dot(transform.rotation, finalRotation) > 0.9999f)
+                {
+                    yield return new WaitForSeconds(moveDelay);
+
+                    //Debug.Log("loop finished");
+
+                    // Make sure we got there
+                    //transform.rotation = finalRotation;
+                    isMoving = false;
+                    destPoint = (destPoint + 1) % points.Length;
+                    
+                    yield break;
+                }
+                yield return null;
+            }
+        }
+
+
+        void TurnToNextPoint()
+        {
+            // Returns if no points have been set up
+            if (points.Length == 0)
+                return;
+
+            IEnumerator coroutine = TurnToPoint(Quaternion.LookRotation(points[destPoint].position - transform.position), rotationSpeed);
+            // Set the agent to turn to the currently selected destination.            
+
+            StartCoroutine(coroutine);
         }
     }
 }
